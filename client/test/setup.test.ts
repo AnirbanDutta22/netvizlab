45d8da10329124f16b730d6fc41bb93a68641871
Anirban Dutta <meanirbandutta22@gmail.com>
2026-08-05 15:49:07 +0530

    fix{ci}: install jsdom, client setup test modify

--- client/test/setup.test.ts
+++ client/test/setup.test.ts
@@ -1,6 +1,8 @@
-import { afterEach } from "vitest";
+import { afterEach, it } from "vitest";
 import { cleanup } from "@testing-library/react";
 
 afterEach(() => {
   cleanup();
 });
+
+it("runs setup", () => {});
